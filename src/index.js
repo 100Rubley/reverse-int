f92d2f019d96ff06814feceaf91fe71f8f5b5326
100Rubley <toxa_98@inbox.ru>
2020-10-10 19:09:22 +0300

    feat: test pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(number) {
+  let reverseNumber
+  if (number > 0) {
+    reverseNumber = number.toString().split('').reverse().join('')
+  } else {
+    number = number * -1
+    reverseNumber = number.toString().split('').reverse().join('')
+  }
+  return reverseNumber
 }
+
